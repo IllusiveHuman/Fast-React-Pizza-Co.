6458980db472f2798c7bd817a0febf55feac867c
IllusiveHuman <149196317+IllusiveHuman@users.noreply.github.com>
2025-06-12 15:37:24 +0300

    Update vite.config.js

--- fast-react-pizza/vite.config.js
+++ fast-react-pizza/vite.config.js
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), mode !== 'production' && eslint(),],
-   
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode !== "production" && eslint(),
+  ].filter(Boolean), // Убирает `false` из массива
+}));
